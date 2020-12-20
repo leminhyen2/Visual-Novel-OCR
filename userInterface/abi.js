b91daffd17543fd3080221355e5139d19c3bdf2f
unknown <leminhyen2@gmail.com>
2020-12-20 15:09:11 +0700

    tidying up userInterface

--- userInterface/abi.js
+++
@@ -1,5 +0,0 @@
-const nodeAbi = require('node-abi')
- 
-//console.log(nodeAbi.getAbi('12.18.3', 'node'))// '51'
-console.log(nodeAbi.getAbi('15.2.0', 'node'))
-console.log(nodeAbi.getAbi('10.1.5', 'electron'))// '51'
